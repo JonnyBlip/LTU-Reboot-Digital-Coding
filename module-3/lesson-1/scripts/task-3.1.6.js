3346691677ef99ea5ec422bec976e49236a06cc0
JonnyBlip <39084783+JonnyBlip@users.noreply.github.com>
2021-02-12 14:20:06 +0000

    added JS - still in progress

---
+++ module-3/lesson-1/scripts/task-3.1.6.js
@@ -0,0 +1,9 @@
+var billtotal = 50.00;
+var onepercent = billtotal / 100;
+var tipamount = 12.5;
+var tip = onepercent * tipamount;
+var total = billtotal + tip;
+
+strMessage = 'Your bill was £' + billtotal + 'you tipped' + tip + 'this mean your total amount is' = total;
+
+console.log(sum);
