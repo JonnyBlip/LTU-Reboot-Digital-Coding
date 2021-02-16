406dcf5333318f7522fc4027662b28df89a51836
JonnyBlip <39084783+JonnyBlip@users.noreply.github.com>
2021-02-16 19:44:08 +0000

    Completed task 3.2.2
    Added JS to combine first last names

---
+++ module-3/lesson-2/scripts/task-3.2.2.js
@@ -0,0 +1,13 @@
+function getfullName(firstName, lastName){
+    let fullName = firstName + ' ' + lastName;
+    console.log(fullName);
+}
+
+var fName = 'Imran';
+var lName = 'Mohammed';
+
+var fName2 = 'Safina';
+var lName2 = 'Shan';
+
+getfullName(fName,lName);
+getfullName(fName2,lName2);
