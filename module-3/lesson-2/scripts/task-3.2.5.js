cb13a0fc41039ffb61187930c804903be5ca5775
JonnyBlip <39084783+JonnyBlip@users.noreply.github.com>
2021-02-16 20:31:53 +0000

    corrected logic

--- module-3/lesson-2/scripts/task-3.2.5.js
+++ module-3/lesson-2/scripts/task-3.2.5.js
@@ -1,12 +1,10 @@
-var temp = 18;
-
 function wearACoat(temperature){
     if(temperature < 0){
-        console.log('Wear a coat');
+        console.log('Wear a coat & hat');
     }
 
     else if(temperature < 10){
-        console.log('Wear a coat & hat');
+        console.log('Wear a coat');
     }
     else if(temperature < 15){
         console.log('Stay inside');
