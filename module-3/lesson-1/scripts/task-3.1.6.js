0061f67f8bd0f9678465bceee39c6ea30b01ee57
JonnyBlip <39084783+JonnyBlip@users.noreply.github.com>
2021-02-16 18:55:00 +0000

    Added Alex js script

--- module-3/lesson-1/scripts/task-3.1.6.js
+++ module-3/lesson-1/scripts/task-3.1.6.js
@@ -1,9 +1,11 @@
-var billtotal = 50.00;
-var onepercent = billtotal / 100;
-var tipamount = 12.5;
-var tip = onepercent * tipamount;
-var total = billtotal + tip;
+var billTotal = 46.44;
+var onePercent = billTotal / 100;
+var tipAmount = 12.5
+var tip = onePercent * tipAmount;
+tip = tip.toFixed(2);
+var total = parseFloat(billTotal) + parseFloat(tip);
+total = total.toFixed(2);
 
-strMessage = 'Your bill was £' + billtotal + 'you tipped' + tip + 'this mean your total amount is' = total;
+var strMessage = 'Your bill was £' + billTotal + ', you tipped £' + tip + ' taking your total bill to £' + total;
 
-console.log(sum);
+console.log(strMessage);
