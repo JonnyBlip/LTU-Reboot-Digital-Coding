b2e65dbeb096997ebb9fbcc76d8bf90688649884
JonnyBlip <39084783+JonnyBlip@users.noreply.github.com>
2021-02-23 20:07:42 +0000

    Completed task 3.4.2

---
+++ module-3/lesson-4/scripts/task-3.4.2.js
@@ -0,0 +1,6 @@
+var arrFavouriteFoods = ['Pizza', 'NasiGoreng', 'Fish & Chips', 'Sandwiches', 'Burger']
+
+for(var counter = 0; counter < arrFavouriteFoods.length; counter++){
+    let food = arrFavouriteFoods[counter];
+    console.log(counter + ': ' + food);
+}
