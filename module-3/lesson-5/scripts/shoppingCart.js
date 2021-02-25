23d3b77e033164424341b4fed7a888cf16fc2f3c
JonnyBlip <39084783+JonnyBlip@users.noreply.github.com>
2021-02-25 20:01:07 +0000

    Completed Task 3.5.1

--- module-3/lesson-5/scripts/shoppingCart.js
+++ module-3/lesson-5/scripts/shoppingCart.js
@@ -1,4 +1,4 @@
-let shoppingCart = [
+let arrShoppingCart = [
   {
     name:"loaf of bread",
     type:"food",
@@ -59,3 +59,20 @@ let shoppingCart = [
   quantity:3,
   price:.4
 }];
+
+function getTotalPrice(arrShoppingCart){
+  var total = 0;
+  let itemPrice = 0;
+  for(var counter=0; counter < arrShoppingCart.length; counter++){
+    var objItem = arrShoppingCart[counter]
+    // console.log(objItem);
+    itemPrice = parseFloat(objItem.quantity) * parseFloat(objItem.price);
+    // console.log(itemPrice);
+    total = total + itemPrice;
+    // console.log(total);
+  }
+  return total.toFixed(2);
+}
+
+let cartTotal = getTotalPrice(arrShoppingCart);
+console.log(cartTotal);
