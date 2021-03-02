e7f395867ad639f48f4ac0b8f53f9e462075db5f
JonnyBlip <39084783+JonnyBlip@users.noreply.github.com>
2021-03-02 18:54:44 +0000

    Completed task 3.5.2

--- module-3/lesson-5/scripts/shoppingCart.js
+++ module-3/lesson-5/scripts/shoppingCart.js
@@ -60,19 +60,53 @@ let arrShoppingCart = [
   price:.4
 }];
 
-function getTotalPrice(arrShoppingCart){
-  var total = 0;
+/**
+ * Returns the total value of the cart (with discounts included)
+ *
+ * @param {array} arrShoppingCart The shopping cart array.
+ * @param {obj} objCoupon TA coupon object.
+ * @return {number} x the total price of the cart with discounts applied.
+ */
+function getTotalPrice(arrShoppingCart=[],objCoupon={}){
+  //setting total of cart to 0 before the loop starts
+  let total = 0;
+  //setting the item price to 0 so it isnt redeclared in the loop
   let itemPrice = 0;
+  //loops through each item in the shopping cart array
   for(var counter=0; counter < arrShoppingCart.length; counter++){
-    var objItem = arrShoppingCart[counter]
+    // console.log(counter);
+    //selects the current cart item using the counter as the array key
+    var objItem = arrShoppingCart[counter];
     // console.log(objItem);
+    //multiply the cost of the item by the quantity of the item
     itemPrice = parseFloat(objItem.quantity) * parseFloat(objItem.price);
     // console.log(itemPrice);
+    //if a coupon has been passed as an argument AND the coupon type is percentage
+    if(objCoupon && objCoupon.type == 'percentage'){
+      //if the coupon category = the current item type OR coupon category = all
+      if(objCoupon.category == objItem.type || objCoupon.category == 'all'){
+        //itemPrice = percentage decrease for item
+        itemPrice = (itemPrice/100) * (100 - objCoupon.value);
+        // console.log(itemPrice);
+      } 
+    }
+    //total = old total + current item price
     total = total + itemPrice;
     // console.log(total);
   }
+  //if a coupon has been passed as an argument AND the coupon type is total
+  if(objCoupon && objCoupon.type == 'total'){
+    //decrease the total by the value of the coupon
+    total = total - objCoupon.value;
+  }
+  //return the total price of the cart (with discount applied where applicable)
   return total.toFixed(2);
 }
-
-let cartTotal = getTotalPrice(arrShoppingCart);
+let objCoupon = {
+  name:'20OFF',
+  value:20,
+  category:'food',
+  type:'percentage'
+};
+let cartTotal = getTotalPrice(arrShoppingCart,objCoupon);
 console.log(cartTotal);
